2f69c73c9193120dafd44ccaeab604bc880df4e0
[SW6] 조은나래 <cenrmomo@gmail.com>
2023-11-17 03:08:06 +0900

    Bug: 컨플릭 버그 수정
    컨플릭 수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import Chat from "./Components/Main/Chat";
 import Footer from "./Common/Footer";
 import ErrorPage from "./Common/Error";
 import AdmimMain from "./Pages/AdminMain";
-import ContentCategory from "./Components/CategoryContent";
-import ContentDetail from "./Pages/ContentDetail/";
+// import ContentDetail from "./Pages/ContentDetail/";
 import ContentCategory from "./Components/Category/CategoryContent";
 import DummyContentDetail from "./Components/Category/dummyContentDetail";
 import { footerEnabledRecoil } from "./Common/CommonAtom";
@@ -32,6 +31,10 @@ function App() {
               <Route path="/" element={<MainPage />} />
               <Route path="/Admin" element={<AdmimMain />} />
               <Route path="/Category" element={<ContentCategory />} />
+              <Route
+                path="/ContentDetail/:idx"
+                element={<DummyContentDetail />}
+              />
               <Route path="/*" element={<ErrorPage to="/" />} />
             </Routes>
           </div>
