a59ed99ad870b2fe0cadf22fa28ce9070b9fda83
woo7i <or2447221@gmail.com>
2023-11-17 16:22:30 +0900

    feat(CommonAtom) : [ADD] 로그인 페이지를 위한 푸터 상태관리  컴포넌트 추가 수정

--- src/Common/CommonAtom.jsx
+++ src/Common/CommonAtom.jsx
@@ -1,6 +1,14 @@
 import { atom } from "recoil";
 
+export const loginPageActiveState = atom({
+  key: "loginPageActiveState",
+  default: false,
+});
 export const footerEnabledRecoil = atom({
   key: "fotterenabledRecoil",
+  default: true,
+});
+export const navEnabledRecoil = atom({
+  key: "navenabledRecoil",
   default: true,
 });
