d3192fc7fc5f9c7b81715cd570d20427247008c9
woo7i <or2447221@gmail.com>
2023-11-17 16:22:07 +0900

    feat(Footer) : [ADD] 로그인 페이지를 위한 푸터 ui 컴포넌트 추가 수정

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -1,9 +1,17 @@
+import { useRecoilValue } from "recoil";
+import { loginPageActiveState } from "../Common/CommonAtom";
+
 export default function Footer() {
+  const loginPageActive = useRecoilValue(loginPageActiveState);
+  const footerClass = loginPageActive
+    ? "bg-new-color"
+    : "bg-white bg-opacity-10";
+
   return (
-    <>
-      <footer className="flex items-center justify-center text-sm h-24 text-white bg-white bg-opacity-10 font-pretendard">
-        Copyright © 2023 watcHere | All Rights Reserved
-      </footer>
-    </>
+    <footer
+      className={`flex items-center justify-center text-sm h-24 text-white ${footerClass} font-pretendard`}
+    >
+      Copyright © 2023 watcHere | All Rights Reserved
+    </footer>
   );
 }
