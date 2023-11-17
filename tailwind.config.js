129d7fb7aa077b010ff8f01c052056a5c2f2cec7
woo7i <or2447221@gmail.com>
2023-11-17 16:24:05 +0900

    feat(tailwind) : [ADD] 로그인 페이지 테일윈드로 사용자 스타일 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-gray": "#4e4e4e",
+        "custom-black": "#000",
+        "custom-light-gray": "#a3a3a3",
+        "custom-hover-gray": "#dedede",
+      },
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(0deg, rgb(44, 44, 44) 0%, rgb(44, 44, 44) 35%, rgba(44, 44, 44, 0.75) 100%);",
+      },
+      backgroundColor: {
+        "new-color": "#2c2c2c",
+        "naver-green": "#2db400",
+      },
+      fontFamily: {
+        "noto-sans-kr": [
+          '"Noto Sans KR"',
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Roboto"',
+          '"Segoe UI"',
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Open Sans",
+          '"Helvetica Neue"',
+          "sans-serif",
+        ],
+      },
+    },
     fontFamily: {
       pretendard: ["Pretendard"],
       pretendardLight: ["Pretendard-Light"],
