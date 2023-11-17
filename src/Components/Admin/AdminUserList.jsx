e1b90b0abb096029722fe5cc432b7a9e63f611d4
[SW6] 조은나래 <cenrmomo@gmail.com>
2023-11-18 04:48:55 +0900

    Design: 반응형 UI 작업
    반응형 UI 용 기초 레이아웃 작업 진행

--- src/Components/Admin/AdminUserList.jsx
+++ src/Components/Admin/AdminUserList.jsx
@@ -1,19 +1,30 @@
+// 코어 라이브러리
+import { useEffect, useState } from "react";
+
+// 더미데이터
 import dummyUserList from "../../resources/userInfo.json";
 import dummyPageNation from "../../resources/pagenition.json";
+
+// 관리자용 유저 스테이터스
 import {
   ADMIN_MENU_USER_ACTIVATE,
   ADMIN_MENU_USER_BAN,
   ADMIN_MENU_USER_WITHDRAW,
 } from "./AdminEnum";
+
+// 그래픽 라이브러리
 import {
   MdKeyboardDoubleArrowLeft,
   MdKeyboardDoubleArrowRight,
   MdKeyboardArrowRight,
   MdKeyboardArrowLeft,
 } from "react-icons/md";
+
+// 커스텀 라이브러리
 import DateFormat, { TIME_FORMATTER_MM_dd_yy } from "../../Common/DateFormat";
-import { useEffect, useState } from "react";
-import { PostData } from "../../Network/Connect";
+
+// 전용 모달 컴포넌트
+import { ChangeUserStateModal, CheckUserInfoModal } from "./AdminModal";
 
 export default function AdminUserList() {
   const [userList, setUserList] = useState([]);
@@ -21,6 +32,7 @@ export default function AdminUserList() {
   const [nowBlock, setNowBlock] = useState(1);
   const [maxBlock, setMaxBlock] = useState(0);
   const [maxPage, setMaxPage] = useState(10);
+  const [userInfo, setUserInfo] = useState({});
   /**
    * 서버에서 유저리스트를 받아오는 함수
    *
@@ -39,6 +51,7 @@ export default function AdminUserList() {
   };
   /**
    * 페이지 이동을 블록단위로 하는 기능
+   *
    * @param {int} block --- 움직일 블록 번호
    */
   const handlePageBlock = (block) => {
@@ -47,6 +60,7 @@ export default function AdminUserList() {
   };
   /**
    * 구현기능 : 페이지 이동
+   *
    * 이슈 : Arrow가 불필요한 시점에 노출되는 문제
    *
    */
@@ -66,20 +80,57 @@ export default function AdminUserList() {
     }
   };
 
-  const putUserActivateStatus = async (uid) => {
-    // 중간에 모달작업 진행해서 사용자의 최종컨펌을 1회 더 받아야함
-    let jsonData = {};
-    jsonData["uId"] = uid;
-    const response = await PostData("url", JSON.stringify(jsonData));
-    console.log(response);
+  const handleUserState = (user, modalId) => {
+    setUserInfo(user);
+    document.getElementById(modalId).showModal();
+    // 네트워크 API 작업 완료 후 작업 진행
+    // putUserActivateStatus(userInfo.id);
   };
+  /**
+   *
+   * @param {int} type 활동중 0 정지 1 탈퇴 2
+   * @returns
+   */
+  const activateStateComponent = (type) => {
+    switch (type) {
+      case ADMIN_MENU_USER_ACTIVATE:
+        return <div className="text-[#0CAF60] bg-[#E7F7EF] p-1">활동 중 </div>;
+      case ADMIN_MENU_USER_BAN:
+        return <div className="text-[#DA0707] bg-[#FFF0F0] p-1">정지 </div>;
+      case ADMIN_MENU_USER_WITHDRAW:
+        return <div className="text-[#2C2C2C] bg-[#F2F2F2] p-1">탈퇴 </div>;
+      default:
+        return;
+    }
+  };
+
+  const activateActionComponent = (type) => {
+    switch (type) {
+      case ADMIN_MENU_USER_ACTIVATE:
+        return (
+          <div className="w-[50%] text-[#DA0707] border rounded-md h-6 flex items-center justify-center border-black">
+            정지하기
+          </div>
+        );
+      case ADMIN_MENU_USER_BAN:
+        return (
+          <div className="w-[50%] text-[#0CAF60]  border rounded-md h-6 flex items-center justify-center border-black">
+            해제하기
+          </div>
+        );
+      default:
+        return;
+    }
+  };
+
   useEffect(() => {
     setMaxBlock(parseInt(dummyPageNation.total_page / maxPage));
     getUserData();
   }, [maxPage]);
+
   return (
     <div className="h-full bg-gray-300 flex flex-col justify-center">
-      <div className="h-2/3 bg-white mx-4">
+      <div className="h-2/3 xl:h-full bg-white mx-4">
         <div className="flex mt-10 ">
           <div className="w-[30%] text-2xl font-bold text-center items-center">
             사용자 관리
@@ -93,34 +144,35 @@ export default function AdminUserList() {
           </div>
         </div>
         <div className="flex items-center text-center border-y-2 py-4 mt-4 text-xs">
-          <div className="w-[20%]">사용자 계정</div>
-          <div className="w-[20%]">가입 일자</div>
-          <div className="w-[20%]">닉네임</div>
-          <div className="w-[20%]">활동 상태</div>
-          <div className="w-[20%]">상태 변경</div>
+          <div className="w-[20%] xl:w-[25%] md:hidden">사용자 계정</div>
+          <div className="w-[20%] xl:w-[25%] md:w-1/3">가입 일자</div>
+          <div className="w-[20%] xl:w-[25%] md:w-1/3">닉네임</div>
+          <div className="w-[20%] xl:w-[25%] md:w-1/3">활동 상태</div>
+          <div className="w-[20%] xl:hidden">상태 변경</div>
         </div>
         <div className="h-[60%] overflow-y-auto">
           {userList?.map((element, idx) => (
             <div
               className="flex items-center text-center mt-1 border-b-2 py-1 text-xs"
+              onClick={() => handleUserState(element, "checkUserInfoModal")}
               key={idx}
             >
-              <div className="w-[20%]">
+              <div className="w-[20%] xl:w-[25%]  md:hidden">
                 <div className="text-black">{element.email} </div>
                 <div>{element.email} </div>
               </div>
-              <div className="w-[20%] text-black font-bold">
+              <div className="w-[20%] xl:w-[25%] md:w-1/3 text-black font-bold">
                 {DateFormat(element.join_date, TIME_FORMATTER_MM_dd_yy)}
               </div>
-              <div className="w-[20%] text-black font-bold">
+              <div className="w-[20%] xl:w-[25%] md:w-1/3 text-black font-bold">
                 {element.nick_name}
               </div>
-              <div className="w-[20%] px-10">
+              <div className="w-[20%] xl:w-[25%] md:w-1/3 px-10">
                 {activateStateComponent(element.activate)}
               </div>
               <div
-                className="w-[20%] flex justify-center px-10 border-10 cursor-pointer"
-                onClick={() => putUserActivateStatus(element.uid)}
+                className="w-[20%] flex justify-center px-10 border-10 cursor-pointer xl:hidden"
+                onClick={() => handleUserState(element, "changeUserStateModal")}
               >
                 {activateActionComponent(element.activate)}
               </div>
@@ -193,39 +245,8 @@ export default function AdminUserList() {
           </div>
         </div>
       </div>
+      <ChangeUserStateModal props={userInfo} />
+      <CheckUserInfoModal props={userInfo} />
     </div>
   );
 }
-
-const activateStateComponent = (type) => {
-  return (
-    <>
-      {type === ADMIN_MENU_USER_ACTIVATE && (
-        <div className="text-[#0CAF60] bg-[#E7F7EF] p-1">활동 중 </div>
-      )}
-      {type === ADMIN_MENU_USER_BAN && (
-        <div className="text-[#DA0707] bg-[#FFF0F0] p-1">정지 </div>
-      )}
-      {type === ADMIN_MENU_USER_WITHDRAW && (
-        <div className="text-[#2C2C2C] bg-[#F2F2F2] p-1">탈퇴 </div>
-      )}
-    </>
-  );
-};
-
-const activateActionComponent = (type) => {
-  return (
-    <>
-      {type === ADMIN_MENU_USER_ACTIVATE && (
-        <div className="w-[50%] text-[#DA0707] border-2 border-black">
-          정지하기
-        </div>
-      )}
-      {type === ADMIN_MENU_USER_BAN && (
-        <div className="w-[50%] text-[#0CAF60] border-2 border-black">
-          해제하기
-        </div>
-      )}
-    </>
-  );
-};
--- src/Pages/AdminMain.jsx
+++ src/Pages/AdminMain.jsx
@@ -1,25 +1,19 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   ADMIN_MENU_USER,
   ADMIN_MENU_REVIEW,
 } from "../Components/Admin/AdminEnum";
 import { GoPeople } from "react-icons/go";
 import { GrDocumentText } from "react-icons/gr";
-import { footerEnabledRecoil } from "../Common/CommonAtom";
-import { useRecoilState } from "recoil";
 import AdminUserList from "../Components/Admin/AdminUserList";
 import AdminReviewList from "../Components/Admin/AdminReviewList";
+
 export default function AdmimMain() {
   const [selectMenu, setSelectNemu] = useState(ADMIN_MENU_USER); // 현재 버튼의 상태를 봅니다.
-  const [, setFooterEnabled] = useRecoilState(footerEnabledRecoil); // 푸터를 활성화 할지 말지를 결정합니다.
-
-  useEffect(() => {
-    setFooterEnabled(false); // 관리자페이지는 기본 푸터가 비활성화이기 때문에 useEffect 에서 푸터를 비활성화 합니다.
-  }, [setFooterEnabled]);
 
   return (
-    <div className="flex justify-center h-screen">
-      <div className="w-[30%] border-2 bg-[#2C2C2C] flex flex-col justify-between">
+    <div className="flex min-h-screen w-[80%] xl:w-full ">
+      <div className="w-1/5 xl:w-96 border-2 bg-[#2C2C2C] flex flex-col justify-between">
         <div>
           <div className="flex items-center justify-center mt-10">
             <div className="text-3xl text-[#9BB0A5]">watc</div>
@@ -27,7 +21,7 @@ export default function AdmimMain() {
             <div className="text-3xl text-[#9BB0A5]">ere</div>
           </div>
           <div
-            className={`flex items-center justify-left mx-10 pl-6 h-10 rounded-md text-md font-semibold mt-10 ${
+            className={`flex items-center justify-left mx-10 px-5 h-10 rounded-md text-md font-semibold mt-10 ${
               selectMenu === ADMIN_MENU_USER
                 ? "bg-[#40ad80] text-white"
                 : "text-gray-400"
@@ -36,14 +30,12 @@ export default function AdmimMain() {
               setSelectNemu(ADMIN_MENU_USER);
             }}
           >
-            <div>
-              <GoPeople />
-            </div>
-            <div className="ml-2">사용자 관리</div>
+            <GoPeople className="md:w-8 md:h-8" />
+            <div className="ml-2 md:hidden">사용자 관리</div>
           </div>
 
           <div
-            className={`flex items-center justify-left mx-10 pl-6 h-10 rounded-md text-md font-semibold ${
+            className={`flex items-center justify-left mx-10 px-5 h-10 rounded-md text-md font-semibold ${
               selectMenu === ADMIN_MENU_REVIEW
                 ? "bg-[#40ad80] text-white"
                 : "text-gray-400"
@@ -52,21 +44,23 @@ export default function AdmimMain() {
               setSelectNemu(ADMIN_MENU_REVIEW);
             }}
           >
-            <div>
-              <GrDocumentText />
-            </div>
-            <div className="ml-2">리뷰 관리</div>
+            <GrDocumentText className="md:w-8 md:h-8" />
+            <div className="ml-2 md:hidden">리뷰 관리</div>
           </div>
         </div>
         <div className="mx-10 mb-5">
-          <div className="flex items-center text-white ">
+          <div className="flex items-center text-white xl:hidden">
             <div>이미지</div>
             <div className="ml-4">
               <div>관리자</div>
               <div>이메일</div>
             </div>
           </div>
-
+          <div className="border-2 flex flex-col items-center justify-center text-white xl:block 2xl:hidden">
+            <div>이미지</div>
+            <div>관리자</div>
+            <div>이메일</div>
+          </div>
           <div>
             <button className="text-black rounded-md p-2 font-semibold bg-white mt-4">
               로그아웃
@@ -74,7 +68,7 @@ export default function AdmimMain() {
           </div>
         </div>
       </div>
-      <div className="w-[70%] border-2">
+      <div className="w-4/5 border-2">
         {selectMenu === ADMIN_MENU_USER && <AdminUserList />}
         {selectMenu === ADMIN_MENU_REVIEW && <AdminReviewList />}
       </div>
