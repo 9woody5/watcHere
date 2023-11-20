2edd30c1a64c926e11515f334280bd9c16b77cd6
woo7i <or2447221@gmail.com>
2023-11-20 15:45:10 +0900

    feat(LoginPage) : 외부 컴포넌트로 활성화 변경하게 되면서 푸터, 네브 활성화 recoil 코드 삭제, 대신 활성화에 따른 스타일 변경 recoil 유지

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -10,23 +10,14 @@ import LoginTitle from "../Components/login/LoginTitle";
 import LoginButton from "../Components/login/LoginButton";
 
 const LoginPage = () => {
-  const [, setFooterEnabled] = useRecoilState(footerEnabledRecoil); // 푸터를 활성화 할지 말지를 결정합니다.
-  const [, setNavEnabled] = useRecoilState(navEnabledRecoil); // nav를 활성화 할지 말지를 결정합니다.
   const [, setLoginPageActive] = useRecoilState(loginPageActiveState);
 
   useEffect(() => {
-    setLoginPageActive(true);
+    setLoginPageActive(false);
     return () => {
-      setLoginPageActive(false);
+      setLoginPageActive(true);
     };
   }, [setLoginPageActive]);
-
-  useEffect(() => {
-    setNavEnabled(false); // 로그인페이지는 기본 네브가 비활성화이기 때문에 useEffect 에서 푸터를 비활성화 합니다.
-  }, [setNavEnabled]);
-  useEffect(() => {
-    setFooterEnabled(true); // 로그인페이지는 기본 푸터가 활성화이기 때문에 useEffect 에서 푸터를 활성화 합니다.
-  }, [setFooterEnabled]);
 
   return (
     <div className="wrap login-select relative h-screen w-screen bg-cover bg-[url('../src/assets/img/login_background_img.jpeg')]">
