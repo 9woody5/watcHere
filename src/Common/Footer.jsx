989c4c47271f6fd6a512e596203a1a0dfc1794b4
woo7i <or2447221@gmail.com>
2023-11-20 15:42:44 +0900

    feat(Footer) : 기존 recoil footer조건을 통한 loginpage활성화 조건 변경, 색상 변경

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -3,10 +3,14 @@ import { loginPageActiveState } from "../Common/CommonAtom";
 
 export default function Footer() {
   const loginPageActive = useRecoilValue(loginPageActiveState);
-  const footerClass = loginPageActive ? "bg-new-color" : "bg-white bg-opacity-10";
+  const footerClass = loginPageActive
+    ? "bg-white bg-opacity-10"
+    : "bg-new-color";
 
   return (
-    <footer className={`flex items-center justify-center text-sm h-16 text-white ${footerClass} font-pretendard`}>
+    <footer
+      className={` border-t border-solid border-1 border-custom-gray flex items-center justify-center text-sm h-16 text-white ${footerClass} font-pretendard`}
+    >
       Copyright © 2023 watcHere | All Rights Reserved
     </footer>
   );
