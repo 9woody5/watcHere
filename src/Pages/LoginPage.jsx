5fede391daa66d070b24b2dc62404d34246aa2af
woo7i <or2447221@gmail.com>
2023-11-20 15:26:30 +0900

    feat(LoginPage) : 주석, 태일윈드 값 수정

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
-import { loginPageActiveState, navEnabledRecoil, footerEnabledRecoil } from "../Common/CommonAtom";
+import {
+  loginPageActiveState,
+  navEnabledRecoil,
+  footerEnabledRecoil,
+} from "../Common/CommonAtom";
 
 import LoginTitle from "../Components/login/LoginTitle";
 import LoginButton from "../Components/login/LoginButton";
 
 const LoginPage = () => {
   const [, setFooterEnabled] = useRecoilState(footerEnabledRecoil); // 푸터를 활성화 할지 말지를 결정합니다.
-  const [, setNavEnabled] = useRecoilState(navEnabledRecoil); // 푸터를 활성화 할지 말지를 결정합니다.
+  const [, setNavEnabled] = useRecoilState(navEnabledRecoil); // nav를 활성화 할지 말지를 결정합니다.
   const [, setLoginPageActive] = useRecoilState(loginPageActiveState);
 
   useEffect(() => {
@@ -26,7 +30,7 @@ const LoginPage = () => {
 
   return (
     <div className="wrap login-select relative h-screen w-screen bg-cover bg-[url('../src/assets/img/login_background_img.jpeg')]">
-      <div className="absolute top-0 left-0 w-full h-[105%] bg-custom-gradient">
+      <div className="absolute top-0 left-0 w-[100%] h-[100%] bg-custom-gradient">
         <main className="flex flex-col items-center justify-center">
           <LoginTitle />
           <div className="btn-wrap  text-white font-noto-sans-kr text-[10.699px] mt-[32.097px] mb-[32.097px] w-[445.781px]">
