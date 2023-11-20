9417e164e00dfb429c78a6d4bbe6874ac5217329
woo7i <or2447221@gmail.com>
2023-11-20 15:58:09 +0900

    feat(Footer) : 조건에 따른 스타일 수정

--- src/Common/Footer.jsx
+++ src/Common/Footer.jsx
@@ -4,12 +4,11 @@ import { loginPageActiveState } from "../Common/CommonAtom";
 export default function Footer() {
   const loginPageActive = useRecoilValue(loginPageActiveState);
   const footerClass = loginPageActive
-    ? "bg-white bg-opacity-10"
-    : "bg-new-color";
-
+    ? "border-t border-solid  border-1 border-custom-gray  bg-new-color  "
+    : "bg-white bg-opacity-10 ";
   return (
     <footer
-      className={` border-t border-solid border-1 border-custom-gray flex items-center justify-center text-sm h-16 text-white ${footerClass} font-pretendard`}
+      className={`${footerClass} flex items-center justify-center text-sm h-16 text-white font-pretendard`}
     >
       Copyright © 2023 watcHere | All Rights Reserved
     </footer>
