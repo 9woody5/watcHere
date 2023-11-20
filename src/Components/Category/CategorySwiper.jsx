e09fa60a2399a02c60c42779eb3fce90add8ddc2
[SW6] 조은나래 <cenrmomo@gmail.com>
2023-11-21 02:41:03 +0900

    Feat: 카테고리 슬라이드 버튼 커스터마이징
    1. 카테고리 버튼 커스터마이징
    2. 고정 가로 사이즈 가변 90%로 복원
    3. 불필요한 CSS 삭제

--- src/Components/Category/CategorySwiper.jsx
+++ src/Components/Category/CategorySwiper.jsx
@@ -1,6 +1,7 @@
 import ThumbnailCard from "./Card";
 import { MovieThumbnailSkeletionComponent } from "../../Common/SkeletonComponent";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { MdOutlineArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
@@ -9,40 +10,81 @@ import "swiper/css/navigation";
 import cssModule from "./CategorySwiper.module.css";
 // import required modules
 import { Autoplay, FreeMode, Navigation, Pagination } from "swiper/modules";
+import { useRef } from "react";
 
 export default function CategorySwiper({ props }) {
   // 스켈레톤 컴포넌트의 배열 개수
   const tempCount = 5;
   const tempArr = Array.from({ length: tempCount }, (_, index) => index);
 
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
   return (
-    <Swiper
-      breakpoints={{
-        720: {
-          slidesPerView: 1,
-        },
-        1080: {
-          slidesPerView: 3,
-        },
-        1599: {
-          slidesPerView: 5,
-        },
-      }}
-      navigation={true}
-      modules={[FreeMode, Pagination, Autoplay, Navigation]}
-      className={cssModule["swiper"]}
-    >
-      {props.length > 0
-        ? props?.map((element) => (
-            <SwiperSlide key={element.id} className={cssModule["swiper-slide"]}>
-              <ThumbnailCard props={element} />
-            </SwiperSlide>
-          ))
-        : tempArr.map((_, idx) => (
-            <SwiperSlide key={idx} className={cssModule["swiper-slide"]}>
-              <MovieThumbnailSkeletionComponent />
-            </SwiperSlide>
-          ))}
-    </Swiper>
+    <>
+      <div className="mt-1 relative -top-20 left-full flex md:hidden">
+        <button
+          ref={prevRef}
+          className="w-12 h-12 -ml-44 rounded-full bg-white flex items-center justify-center"
+        >
+          <MdOutlineArrowBackIosNew className="text-3xl" />
+        </button>
+        <button
+          ref={nextRef}
+          className="w-12 h-12 ml-10 rounded-full bg-[#40AD80] flex items-center justify-center"
+        >
+          <MdArrowForwardIos className="text-white text-3xl" />
+        </button>
+      </div>
+
+      {/* <div className="mt-1 relative flex md:block">
+        <button
+          ref={prevRef}
+          className="w-12 h-12 rounded-full bg-white flex items-center justify-center"
+        >
+          <MdOutlineArrowBackIosNew className="text-3xl" />
+        </button>
+        <button
+          ref={nextRef}
+          className="w-12 h-12 rounded-full bg-[#40AD80] flex items-center justify-center"
+        >
+          <MdArrowForwardIos className="text-white text-3xl" />
+        </button>
+      </div> */}
+
+      <Swiper
+        breakpoints={{
+          767: {
+            slidesPerView: 1,
+          },
+          1023: {
+            slidesPerView: 3,
+          },
+          1279: {
+            slidesPerView: 5,
+          },
+        }}
+        navigation={{
+          nextEl: nextRef.current,
+          prevEl: prevRef.current,
+        }}
+        modules={[FreeMode, Pagination, Autoplay, Navigation]}
+        className={cssModule["swiper"]}
+      >
+        {props.length > 0
+          ? props?.map((element) => (
+              <SwiperSlide
+                key={element.idx}
+                className={cssModule["swiper-slide"]}
+              >
+                <ThumbnailCard props={element} />
+              </SwiperSlide>
+            ))
+          : tempArr.map((_, idx) => (
+              <SwiperSlide key={idx} className={cssModule["swiper-slide"]}>
+                <MovieThumbnailSkeletionComponent />
+              </SwiperSlide>
+            ))}
+      </Swiper>
+    </>
   );
 }
