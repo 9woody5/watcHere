af2153712ae830449afe3aed7ca183a92a35a0b0
woo7i <or2447221@gmail.com>
2023-11-22 18:19:20 +0900

    feat(UserReview) : 유저 리뷰 추후 api 토대로 ui 개선

--- src/Components/Users/UsersReview.jsx
+++ src/Components/Users/UsersReview.jsx
@@ -1,3 +1,9 @@
-const UsersReview = () => {};
+const UsersReview = () => {
+  return (
+    <div className="border-solid border border-custom-light-gray rounded-sm bg-custom-middle-gray w-[60%] h-[auto] ">
+      {/* 추후 api 받고 DB에 저장된 사용자가 작성한 리뷰 토대로 가져와서 ui 제작 */}
+    </div>
+  );
+};
 
 export default UsersReview;
