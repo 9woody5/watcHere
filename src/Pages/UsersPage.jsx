b3c0b736b8496d1a259dc8e6413413121be9014a
woo7i <or2447221@gmail.com>
2023-11-22 18:22:41 +0900

    feat(UserPage) : 유저 페이지 ui 구현

--- src/Pages/UsersPage.jsx
+++ src/Pages/UsersPage.jsx
@@ -4,8 +4,16 @@ import UsersReview from "../Components/Users/UsersReview";
 
 const UsersPage = () => {
   return (
-    <div className="flex justify-center">
-      <UsersProfile />
+    <div className=" w-[full] h-[full]">
+      <section className="flex flex-col items-center justify-center text-white mt-9">
+        <UsersProfile />
+      </section>
+      <section className="flex flex-col items-center justify-center mt-5">
+        <UsersBookmark />
+      </section>
+      <section className="flex flex-col items-center justify-center mt-5">
+        <UsersReview />
+      </section>
     </div>
   );
 };
