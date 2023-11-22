51a1029f041fb1c37872c6619ed7b760736d68ee
woo7i <or2447221@gmail.com>
2023-11-22 18:21:04 +0900

    feat(UserProfile) : 유저프로필 컴포넌트 ui 구현

--- src/Components/Users/UsersProfile.jsx
+++ src/Components/Users/UsersProfile.jsx
@@ -1,15 +1,75 @@
+import { FaGear } from "react-icons/fa6";
+
 const UsersProfile = () => {
+  const getProfileBackgroundImage = () => {
+    return 'url("src/assets/img/temporarily-profile-img.jpeg")';
+  };
+
+  const profilePhotoStyle = {
+    backgroundImage: getProfileBackgroundImage(),
+    position: "absolute",
+    transform: "translate(50%, -25%)",
+    backgroundSize: "135px",
+    backgroundRepeat: "no-repeat",
+    width: "135px",
+    height: "135px",
+  };
+
   return (
-    <section className="border border-white rounded-sm bg-custom-middle-gray w-[60%] h-[auto] ">
-      <div className="profile-setting"></div>
-      <div className="user-profile flex justify-center font-normal text-center text-sm h-24 text-white">
-        <div className="user-profile-photo"></div>
-        <div className="user-profile-name">
-          <h2>유저 닉네임 : EliceRabbit</h2>
-          <p>유저 이메일 : elise@elise.com</p>
+    <div className=" relative flex items-center justify-between border-solid border-2 border-custom-light-gray rounded-sm bg-custom-middle-gray w-[60%] h-[250px] ">
+      <div className="user-profile w-[33%] ">
+        <div className="flex flex-col flex-shrink-1 items-center justify-center user-profile-photo   w-[50%] ">
+          <div
+            className=" rounded-full "
+            style={{ ...profilePhotoStyle }}
+          ></div>
+          <button
+            className="btn btn-ghost avatar hover:bg-zinc-700"
+            style={{
+              position: "absolute",
+              transform: "translate(65%, 150%)",
+            }}
+          >
+            프로필 수정
+          </button>
         </div>
       </div>
-    </section>
+      <div className="flex flex-col flex-grow-3 w-[33%] font-normal text-center text-sm text-white">
+        <h2>유저 닉네임</h2>
+        <p>EliceRabbit</p>
+        <br />
+        <h2>유저 이메일</h2>
+        <p>elise@elise.com</p>
+      </div>
+      <div className="flex items-center justify-end dropdown dropdown-end profile-setting w-[33%]">
+        <label
+          tabIndex={0}
+          className="btn btn-ghost btn-circle avatar hover:bg-zinc-700"
+          style={{
+            position: "absolute",
+            transform: "translate(-50%, -170%)",
+          }}
+        >
+          <button>
+            <FaGear size={22} />
+          </button>
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-28 text-black"
+        >
+          <li>
+            <button>로그아웃</button>
+          </li>
+          <li>
+            <button>계정변경</button>
+          </li>
+          <li>
+            <button>회원탈퇴</button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
