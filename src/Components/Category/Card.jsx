3a2c652602ec5a9b9e975115e21882053183257b
[SW6] 조은나래 <cenrmomo@gmail.com>
2023-11-24 00:17:03 +0900

    Fix: 레이아웃 수정 및 버그 수정
    1. 카드 리스트 레이아웃 수정
    2. 카드 즐겨찾기 버튼 클릭시 모달창 뜨도록 수정
    3. 카테고리 api 연결 및 랜덤 데이터 불러오도록 수정
    4. 최소 사이즈시 마우스 오버 영역이 크게 잡히는 문제 수정
    5. 카드 리스트 정렬 css 수정

--- src/Components/Category/Card.jsx
+++ src/Components/Category/Card.jsx
@@ -13,13 +13,12 @@ import { AddedFavoritesModal } from "./CategoryModal";
  */
 
 export default function ThumbnailCard({ props }) {
-  console.log(props);
   const { year, id, title, poster_path } = props;
   return (
-    <div className="">
-      <div className="rounded group">
+    <div className="w-full">
+      <div className="rounded group flex items-center justify-center lg:mx-50 ">
         <img src={poster_path} className="group-hover:opacity-50" alt="" />
-        <div className="w-64 h-80 hidden group-hover:block absolute transform -translate-y-full text-white">
+        <div className="w-64 h-80 hidden group-hover:block absolute  text-white ">
           <div className="h-full py-4 flex flex-col justify-between">
             <div>
               <div className="text-xl font-bold w-full bg-[#00B9AE] text-black">
@@ -31,7 +30,12 @@ export default function ThumbnailCard({ props }) {
               <div>리뷰 (49개)</div>
               <div className="mt-2 px-4 w-full flex items-center justify-between">
                 <div className="flex items-center justify-center bg-gray-700 w-12 h-12">
-                  <RiBookmarkFill className="w-10 h-10" />
+                  <RiBookmarkFill
+                    className="w-10 h-10"
+                    onClick={() =>
+                      document.getElementById("addFavoritesModal").showModal()
+                    }
+                  />
                 </div>
                 <div className="flex items-center justify-center rounded-md bg-[#00B9AE] text-black font-bold text-xl w-2/3 h-12">
                   <Link to={`/contentDetail/${id}`}> 상세보기 </Link>
@@ -41,13 +45,14 @@ export default function ThumbnailCard({ props }) {
           </div>
         </div>
       </div>
-      <div className=" flex items-center text-center text-xl font-bold mt-2">
+      <div className="text-xl font-bold mt-2 flex items-center justify-center">
         {title}
       </div>
       <div className="flex mt-1">
         <div className="">{year}</div>
-        <div className="ml-2">104</div>
+        <div className="ml-2"></div>
       </div>
+      <AddedFavoritesModal props={props} />
     </div>
   );
 }
