93441d97c96ec0e294dc8e3993497c64841265b0
woo7i <or2447221@gmail.com>
2023-11-27 11:20:57 +0900

    feat(OAuth) : 리다이렉트 경로 http -> https 프로토콜 수정

--- src/Components/Login/OAuth.js
+++ src/Components/Login/OAuth.js
@@ -1,15 +1,15 @@
-// const REDIRECT_URL = `http://kdt-sw-6-team05.elicecoding.com/oauth/redirect`;
+// const REDIRECT_URL = `https://kdt-sw-6-team05.elicecoding.com/oauth/redirect`;
 const REDIRECT_URL = `http://localhost:5173/oauth/redirect`;
 
 const KAKAO = {
-  AUTH_URL: `http://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URL}`,
+  AUTH_URL: `https://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/kakao?redirect_uri=${REDIRECT_URL}`,
 };
 
 const GOOGLE = {
-  AUTH_URL: `http://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/google?redirect_uri=${REDIRECT_URL}`,
+  AUTH_URL: `https://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/google?redirect_uri=${REDIRECT_URL}`,
 };
 const NAVER = {
-  AUTH_URL: `http://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/naver?redirect_uri=${REDIRECT_URL}`,
+  AUTH_URL: `https://kdt-sw-6-team05.elicecoding.com/oauth2/authorization/naver?redirect_uri=${REDIRECT_URL}`,
 };
 
 export { KAKAO, GOOGLE, NAVER };
