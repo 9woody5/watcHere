801167d910ac6ff02ce72af55e92d1063c0ac4ee
woo7i <or2447221@gmail.com>
2023-11-28 18:56:24 +0900

    feat(UserProfile) : 모달로 값 전달 시 바로 수정될 수 있게 로직 수정

--- src/Components/Users/UsersProfile.jsx
+++ src/Components/Users/UsersProfile.jsx
@@ -1,47 +1,106 @@
+import { useState, useEffect } from "react";
+import UserFavoriteContents from "./UserFavoriteContents";
+import ProfileEditModal from "./ProfileEditModal";
+import { useRecoilState } from "recoil";
+import { userInfoState } from "../../Common/CommonAtom";
 import { FaGear } from "react-icons/fa6";
+import { BiSolidUserCircle } from "react-icons/bi";
 
 const UsersProfile = () => {
-  const getProfileBackgroundImage = () => {
-    return 'url("src/assets/img/temporarily-profile-img.jpeg")';
-  };
+  // 사용자 정보를 저장할 상태
+  const [userInfo, setUserInfo] = useRecoilState(userInfoState);
 
-  const profilePhotoStyle = {
-    backgroundImage: getProfileBackgroundImage(),
-    position: "absolute",
-    transform: "translate(50%, -25%)",
-    backgroundSize: "135px",
-    backgroundRepeat: "no-repeat",
-    width: "135px",
-    height: "135px",
+  useEffect(() => {
+    // 사용자 정보를 불러오는 함수
+    const fetchUserInfo = async () => {
+      const token = localStorage.getItem("token"); // 로컬 스토리지에서 토큰 가져오기
+      try {
+        const response = await fetch(
+          "https://kdt-sw-6-team05.elicecoding.com/api/v1/users/me",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        const data = await response.json();
+        if (response.ok) {
+          setUserInfo({
+            nickname: data.nickname,
+            email: data.email,
+            profile_image: data.profile_image,
+          });
+        } else {
+          // 에러 처리
+          console.error("사용자 정보를 불러오는데 실패했습니다.");
+        }
+        console.log("데이터", data);
+      } catch (error) {
+        console.error("오류 발생:", error);
+      }
+    };
+
+    fetchUserInfo();
+  }, []);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const updateUserInfo = (newUserInfo) => {
+    setUserInfo(newUserInfo);
+    // API 호출 등으로 서버에 정보 업데이트
   };
 
   return (
     <div className=" relative flex items-center justify-between border-solid border-2 border-custom-light-gray rounded-sm bg-custom-middle-gray w-[60%] h-[250px] ">
-      <div className="user-profile w-[33%] ">
+      <div className="user-profile w-[25%] ">
         <div className="flex flex-col flex-shrink-1 items-center justify-center user-profile-photo   w-[50%] ">
-          <div
-            className=" rounded-full "
-            style={{ ...profilePhotoStyle }}
-          ></div>
+          {userInfo.profile_image ? (
+            <img
+              src={userInfo.profile_image}
+              alt="User Profile"
+              className="absolute translate-x-[50%] translate-y-[-25%] rounded-full"
+              style={{ width: "8vw", height: "8vw" }}
+            />
+          ) : (
+            <BiSolidUserCircle
+              size="8vw"
+              color="9bb0a5"
+              className="absolute translate-x-[50%] translate-y-[-25%]"
+            />
+          )}
+
           <button
+            onClick={() => setIsModalOpen(true)}
             className="btn btn-ghost avatar hover:bg-zinc-700"
             style={{
               position: "absolute",
-              transform: "translate(65%, 150%)",
+              transform: "translate(60%, 150%)",
             }}
           >
-            프로필 수정
+            프로필 편집
           </button>
+
+          <ProfileEditModal
+            isOpen={isModalOpen}
+            onRequestClose={() => setIsModalOpen(false)}
+            userInfo={userInfo}
+            setUserInfo={setUserInfo}
+          />
         </div>
       </div>
-      <div className="flex flex-col flex-grow-3 w-[33%] font-normal text-center text-sm text-white">
-        <h2>닉네임</h2>
-        <p>EliceRabbit</p>
+
+      <div className="flex flex-col  flex-grow-3 w-[15%] font-normal text-start text-sm text-white translate-x-[0%] translate-y-[-35%]">
+        <p className="text-base font-bold">닉네임</p>
+        <p>{userInfo.nickname}</p>
         <br />
-        <h2>이메일</h2>
-        <p>elise@elise.com</p>
+        <p className="text-base font-bold">이메일</p>
+        <p>{userInfo.email}</p>
       </div>
-      <div className="flex items-center justify-end dropdown dropdown-end profile-setting w-[33%]">
+      <div className="favorite-profile w-[40%]">
+        <UserFavoriteContents />
+      </div>
+      <div className="flex items-center justify-end dropdown dropdown-end profile-setting w-[10%]">
         <label
           tabIndex={0}
           className="btn btn-ghost btn-circle avatar hover:bg-zinc-700"
