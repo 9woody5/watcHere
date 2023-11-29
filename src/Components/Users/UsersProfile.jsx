b4fa7152ff36c89760ea1baf772779105054f54a
woo7i <or2447221@gmail.com>
2023-11-29 20:19:25 +0900

    feat(UsersProfile) : 탈퇴처리 기능 추가

--- src/Components/Users/UsersProfile.jsx
+++ src/Components/Users/UsersProfile.jsx
@@ -44,24 +44,61 @@ const UsersProfile = () => {
     fetchUserInfo();
   }, []);
 
+  const handleUserDeletion = async () => {
+    // 경고창 표시
+    const isConfirmed = window.confirm("정말로 탈퇴하시겠습니까?");
+
+    if (isConfirmed) {
+      const token = localStorage.getItem("token"); // 토큰 가져오기
+      try {
+        const response = await fetch(
+          "https://kdt-sw-6-team05.elicecoding.com/api/v1/users/me",
+          {
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          // 탈퇴 처리 성공
+          alert("탈퇴되었습니다.");
+
+          // 로컬 스토리지에서 토큰 삭제
+          localStorage.removeItem("token");
+
+          // 메인 페이지로 리다이렉트
+          window.location.href = "/";
+        } else {
+          // 에러 처리
+          alert("탈퇴 처리 중 문제가 발생했습니다.");
+        }
+      } catch (error) {
+        console.error("오류 발생:", error);
+        alert("오류가 발생했습니다.");
+      }
+    }
+  };
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className=" relative flex items-center justify-between border-solid border-2 border-custom-light-gray rounded-sm bg-custom-middle-gray w-[60%] h-[250px] ">
+    <div className=" relative flex items-center justify-between border-solid border-2 border-custom-light-gray rounded-sm bg-custom-middle-gray w-[60%] h-[350px] ">
       <div className="user-profile w-[25%] ">
-        <div className="flex flex-col flex-shrink-1 items-center justify-center user-profile-photo   w-[50%] ">
+        <div className="flex flex-col flex-shrink-1 items-center justify-center user-profile-photo  w-[50%] ">
           {userInfo.profile_image ? (
             <img
               src={userInfo.profile_image}
               alt="User Profile"
-              className="absolute translate-x-[70%] translate-y-[-8%] rounded-full"
+              className="absolute translate-x-[50%] translate-y-[-8%] rounded-full"
               style={{ width: "10vw", height: "10vw" }}
             />
           ) : (
             <BiSolidUserCircle
               size="10vw"
               color="9bb0a5"
-              className="absolute translate-x-[70%] translate-y-[-8%]"
+              className="absolute translate-x-[50%] translate-y-[-8%]"
             />
           )}
 
@@ -86,30 +123,27 @@ const UsersProfile = () => {
         <UserFavoriteContents />
       </div>
 
-      <div className="flex items-center justify-end dropdown dropdown-end profile-setting w-[10%]">
+      <div className=" dropdown dropdown-end profile-setting w-[10%] translate-y-[-250%] ">
         <label
           tabIndex={0}
-          className="btn btn-ghost btn-circle avatar hover:bg-zinc-700"
-          style={{
-            position: "absolute",
-            transform: "translate(-80%, -170%)",
-          }}
+          className=" relative btn btn-ghost btn-circle avatar hover:bg-zinc-70 "
         >
-          <button>
+          <button className=" ">
             <FaGear size={22} />
           </button>
+
+          <ul
+            tabIndex={0}
+            className=" absolute top-full left-0 menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-28 text-black"
+          >
+            <li>
+              <button onClick={() => setIsModalOpen(true)}>프로필 편집</button>
+            </li>
+            <li>
+              <button onClick={handleUserDeletion}>회원탈퇴</button>
+            </li>
+          </ul>
         </label>
-        <ul
-          tabIndex={0}
-          className="menu menu-sm dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-28 text-black"
-        >
-          <li>
-            <button onClick={() => setIsModalOpen(true)}>프로필 편집</button>
-          </li>
-          <li>
-            <button>회원탈퇴</button>
-          </li>
-        </ul>
       </div>
     </div>
   );
