b93ae0ab4f335f228d625c5089ae054dd8b65999
woo7i <or2447221@gmail.com>
2023-12-01 19:56:53 +0900

    feat(LoginTitle) : 로그인 UI 추가 수정

--- src/Components/Login/LoginTitle.jsx
+++ src/Components/Login/LoginTitle.jsx
@@ -14,8 +14,9 @@ const LoginTitle = () => {
       <Link to={"/"} className="w-[300px] mx-[-3rem] my-10 mb-12">
         <img src={logo} alt="" />
       </Link>
-      <h3 className="flex flex-col gap-2 head-title font-pretendard relative  text-white text-[1.5rem]">
-        <span>반가워요!🥳</span>
+      <h3 className="flex flex-col gap-2 head-title relative  text-white text-[1.5rem] font-pretendardBold">
+        <span>반가워요! 🥳</span>
+        <br />
         <span className="font-pretendardBold">계정을 선택해주세요.</span>
       </h3>
     </div>
